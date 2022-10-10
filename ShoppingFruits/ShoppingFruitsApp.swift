35201ed9b24bc43f93083fd395d28bbd7381241c
Aishwarya Iyer <aradhakrishnaniye@deloitte.com>
2022-10-10 11:58:26 +0530

    Initial Commit

---
+++ ShoppingFruits/ShoppingFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingFruitsApp.swift
+//  ShoppingFruits
+//
+//  Created by Radhakrishnan Iyer, Aishwarya on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
